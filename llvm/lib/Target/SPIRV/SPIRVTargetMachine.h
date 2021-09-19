4a8ef66f87f76b0c7c755ffc9627d619137e0fd5
Iliya Diyachkov <iliya.diyachkov@intel.com>
2021-09-20 00:22:28 +0300

    [SPIR-V] add isNoopAddrSpaceCast, fix typo in literal-struct.ll test

--- llvm/lib/Target/SPIRV/SPIRVTargetMachine.h
+++ llvm/lib/Target/SPIRV/SPIRVTargetMachine.h
@@ -38,6 +38,10 @@ public:
   TargetLoweringObjectFile *getObjFileLowering() const override {
     return TLOF.get();
   }
+
+  bool isNoopAddrSpaceCast(unsigned SrcAS, unsigned DestAS) const override {
+    return true;
+  }
 };
 } // namespace llvm
 
